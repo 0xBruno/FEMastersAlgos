b6f4ffa26b96dac46d85ec08bc5fd002c6006754
bruno <bruno>
2023-08-17 20:25:34 -0500

    bubble sort comments

--- sort.go
+++ sort.go
@@ -2,8 +2,13 @@ package main
 
 func BubbleSort(unsorted []int) []int {
 
+	// decrementing array
 	for length := len(unsorted) - 1; length != 0; length-- {
+
+		// loop over entire shrinking array
 		for i := 0; i < length; i++ {
+
+			// if left is larger than right, swap
 			if unsorted[i] > unsorted[i+1] {
 				smaller := unsorted[i+1]
 				larger := unsorted[i]
