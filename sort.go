4dc79cd984b1459afe62544f35ebee7267c20e53
bruno <bruno>
2023-08-22 21:27:27 -0500

    added quicksort

--- sort.go
+++ sort.go
@@ -20,7 +20,44 @@ func BubbleSort(unsorted []int) []int {
 	return unsorted
 }
 
-// QuickSort divide & conquer
-func QuickSort() {
+func partition(arr *[]int, low int, high int) int {
+	pivot := (*arr)[high]
+	index := low - 1
 
+	for i := low; i < high; i++ {
+		if (*arr)[i] <= pivot {
+			index++
+			tmp := (*arr)[i]
+			(*arr)[i] = (*arr)[index]
+			(*arr)[index] = tmp
+		}
+	}
+
+	// swap pivot with pivot index
+	index++
+	(*arr)[high] = (*arr)[index]
+	(*arr)[index] = pivot
+
+	return index
+}
+
+func qs(arr *[]int, low int, high int) {
+	if low >= high {
+		return
+	}
+
+	pivotIndex := partition(arr, low, high)
+
+	qs(arr, low, pivotIndex-1)
+	qs(arr, pivotIndex+1, high)
+
+}
+
+// QuickSort divide & conquer
+func QuickSort(arr *[]int) {
+	// this one was definitely a straight port over and need to study it more
+	// two steps
+	// 1. partition
+	// 2. quick sort
+	qs(arr, 0, len(*arr)-1)
 }
--- sort_test.go
+++ sort_test.go
@@ -26,3 +26,13 @@ func TestBubbleSort(t *testing.T) {
 		t.Errorf("WANT %v", want)
 	}
 }
+
+func TestQuickSort(t *testing.T) {
+	arr := []int{9, 3, 7, 4, 69, 420, 42}
+	QuickSort(&arr)
+
+	if !reflect.DeepEqual(arr, []int{3, 4, 7, 9, 42, 69, 420}) {
+		t.Fatal(arr)
+	}
+
+}
